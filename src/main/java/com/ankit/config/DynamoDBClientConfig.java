92cd9e99b8693e7941ccee55da8cd9d15724bf0c
ankitfs <ankitagarwal.fs@gmail.com>
2023-12-29 16:49:14 +0530

    DynamoDBClientConfig: the old approach to validate AWS Credentials

--- src/main/java/com/ankit/config/DynamoDBClientConfig.java
+++ src/main/java/com/ankit/config/DynamoDBClientConfig.java
@@ -16,23 +16,17 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 @PropertySource("classpath:application.properties")
 public class DynamoDBClientConfig {
 
-    @Value("${application.accessKey}")
-    private String accessKey;
-
-    @Value("${application.secretKey}")
-    private String secretKey;
-
     @Bean
     public DynamoDbClient getDBClient() {
         //Instead of loading AWS Credentials from system config folder (via ProfileCredentialsProvider),
         //We are loading the credentials from
         //properties file (via AwsBasicCredentials)
-        //ProfileCredentialsProvider pcp = ProfileCredentialsProvider.create();
-        StaticCredentialsProvider scp = StaticCredentialsProvider.
-                create(AwsBasicCredentials.create(accessKey, secretKey));
+        ProfileCredentialsProvider pcp = ProfileCredentialsProvider.create();
+        //StaticCredentialsProvider scp = StaticCredentialsProvider.
+        //        create(AwsBasicCredentials.create(accessKey, secretKey));
         Region region = Region.AP_SOUTH_1;
         DynamoDbClient ddb = DynamoDbClient.builder()
-                .credentialsProvider(scp)
+                .credentialsProvider(pcp)
                 .region(region)
                 .build();
         return ddb;
